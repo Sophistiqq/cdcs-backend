8a4a0de10eaeb8e614d0e4a0b98822bb46ac3d40
Roi Borromeo <roi.for.school@gmail.com>
2025-06-08 02:51:36 +0000

    Initial Commit before Deployment

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { Elysia, t } from "elysia";
+import cors from "@elysiajs/cors"
+const FILE = '/tmp/cdcs.json'
+const PORT = 8080
+async function readClips() {
+  try {
+    const file = Bun.file(FILE)
+    const text = await file.text();
+    return JSON.parse(text)
+  } catch {
+    return []
+  }
+}
+
+async function writeClips(clips: any[]) {
+  await Bun.write(FILE, JSON.stringify(clips.slice(0, 20)))
+}
+
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Hello, the app is running")
+  .post('/clipboard', async ({ body }) => {
+    const { text } = body;
+    console.log(text)
+    const clips = await readClips();
+    clips.unshift({ text, time: Date.now() })
+    await writeClips(clips)
+    return 'OK';
+  }, {
+    body: t.Object({
+      text: t.String()
+    })
+  })
+  .get('/clipboard', async () => {
+    return await readClips();
+  })
+  .listen(PORT);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
